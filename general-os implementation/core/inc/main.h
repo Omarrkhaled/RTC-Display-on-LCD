6c93f82b17a4290c2eba989536fca38013c1589c
Omar Khaled <omarkhalid881@gmail.com>
2022-11-04 20:09:27 +0200

    Add files via upload

---
+++ general-os implementation/core/inc/main.h
@@ -0,0 +1,15 @@
+/*
+ * main.h
+ *
+ *  Created on: Nov 2, 2022
+ *      Author: omark
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include "stm32f429xx.h"
+#include "stdint.h"
+#include "lcd.h"
+
+#endif /* MAIN_H_ */
